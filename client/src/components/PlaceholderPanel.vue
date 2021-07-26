576d40d8cd7b1f11961f1c05c92f814cebabe15f
vlad <vvidavsky@gmail.com>
2021-07-26 17:05:46 +0300

    Adding placeholder component

---
+++ client/src/components/PlaceholderPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="sol-placeholder-overlay">
+    <h6 class="text-blue-grey-3 text-subtitle1">
+      {{ text ? text : $t('placeholder_panel.no_data') }}
+    </h6>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NoWidgetData',
+  props: ['text'],
+};
+</script>
--- client/src/components/Preloader.vue
+++ client/src/components/Preloader.vue
@@ -13,19 +13,6 @@ export default defineComponent({
 });
 </script>
 <style lang="scss">
-.sol-preloader-overlay {
-  opacity: 0.9;
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  z-index: 10;
-  top: 0;
-  left: 0;
-  background: white;
-}
 .sc-pr-rounded {
   border-radius: 15px;
 }
