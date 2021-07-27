fa1ca7525541628907647ed9ef607a9f7501da39
vlad <vvidavsky@gmail.com>
2021-07-27 14:26:51 +0300

    Creating AddressAutocomplete.vue

--- client/src/components/dialogs/CreateDeviceDialog.vue
+++ client/src/components/dialogs/CreateDeviceDialog.vue
@@ -1,57 +1,32 @@
 <template>
   <q-dialog persistent v-model="showDialog">
-    <q-card class="sol-dialog-default overflow-hidden">
+    <q-card class="sol-dialog-default overflow-hidden sol-add-device-dlg">
       <div class="text-h6">{{ $tm('add_device') }}</div>
 
-      <q-card-section class="sol-dialog-content">
-        Lorem ipsum dolor sit amet consectetur
-        Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut, natus minima, porro labore.
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut,
-        natus minima, porro labore.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro.
-        Rerum blanditiis perferendis totam, ea at
-        omnis vel numquam exercitationem aut, natus minima, porro labore.
+      <q-card-section class="sol-dialog-content sol-form-grid">
+        <q-select filled :label="$t('pergola_colors')" />
+        <q-select filled :label="$t('rafter_size')" />
+        <q-select filled :label="$t('louvered_size')" />
+        <q-select filled :label="$t('num_motors')" />
+        <q-input filled :label="$t('technician_name')" v-model="formData.technician_name" />
+        <q-input filled :label="$t('technician_company')" v-model="formData.technician_company" />
+        <q-input square filled :label="$tm('install_date')"
+                 v-model="formData.installation_date"
+                 mask="date">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date v-model="formData.installation_date" :options="getDatePickerOptions">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+        <q-btn flat class="bg-grey-3" :label="$t('add_photo')" icon="photo_camera" size="18px" />
+        <address-autocomplete :model="formData" />
       </q-card-section>
 
       <div class="sol-dialog-footer">
@@ -63,25 +38,38 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
+import { date } from 'quasar';
+import AddressAutocomplete from 'components/inputs/AddressAutocomplete';
 
 export default defineComponent({
   name: 'CreateDeviceDialog',
+  components: {
+    AddressAutocomplete,
+  },
   setup() {
     const showDialog = ref(false);
+    const formData = reactive({
+      technician_name: null,
+      technician_company: null,
+      installation_date: null,
+      device_image_url: null,
+      address: null,
+      lat: null,
+      long: null,
+    });
+    const getDatePickerOptions = (d) => d >= date.formatDate(Date.now(), 'YYYY/MM/DD');
     return {
       showDialog,
+      formData,
+      getDatePickerOptions,
     };
   },
-  // data() {
-  //   return {
-  //     showDialog: false,
-  //   };
-  // },
-  // methods: {},
 });
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.sol-add-device-dlg {
+  width: 500px;
+}
 </style>
---
+++ client/src/components/inputs/AddressAutocomplete.vue
@@ -0,0 +1,114 @@
+<template>
+  <q-select filled
+            class="sc-address-autocomplete"
+            color="blue-13"
+            :disable="disable"
+            use-input
+            fill-input
+            option-value="val"
+            option-label="place_name"
+            :model-value="model.address"
+            @input-value="getResults"
+            @input="selectAddress"
+            :options="options"
+            ref="addressAutocomplete"
+            :loading="gettingResults"
+            :placeholder="$t('address')" />
+</template>
+
+<script>
+import { debounce } from 'quasar';
+import MapboxAPI from 'src/api/mapbox';
+
+export default {
+  name: 'Autocomplete',
+  props: {
+    model: {
+      type: Object,
+      required: true,
+    },
+    disable: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      avoidQuery: false,
+      options: [],
+      getResults: null,
+      gettingResults: false,
+    };
+  },
+  mounted() {
+    this.avoidQuery = true;
+    // eslint-disable-next-line no-return-assign
+    setTimeout(() => this.avoidQuery = false, 1000);
+  },
+  created() {
+    /**
+     * Мы инициализируем данную функцию здесь, а не в methods, потому что
+     * только таким образом у нас есть доступ к контексту this изза дебаунса.
+     * Синтаксис methods не позволяет написать функцию так, чтобы this был доатупен.
+     * Функция запрашивающая варианты адресов из API MapBox согласно вписанному пользователем
+     * тексту. После чего варианты добавляются в компонент select для выбора пользователем.
+     * Влад. 10/12/20
+     * @type {(function(*=): void) & {cancel(): void}}
+     */
+    this.getResults = debounce((val) => {
+      if (val && val.length > 2 && !this.avoidQuery) {
+        // eslint-disable-next-line vue/no-mutating-props
+        this.model.address = val;
+        console.log(val);
+        this.gettingResults = true;
+        MapboxAPI.getSuggestions(val)
+          .then((addresses) => {
+            this.options = null;
+            if (addresses.data && addresses.data.features) {
+              this.options = addresses.data.features;
+              setTimeout(() => {
+                this.gettingResults = false;
+                this.$refs.addressAutocomplete.showPopup();
+              }, 1000);
+            } else {
+              this.options = [];
+            }
+            // eslint-disable-next-line no-return-assign
+          }).catch((e) => {
+            this.getResults = false;
+            this.options = null;
+          });
+      } else {
+        this.options = [];
+        // this.$emit('clear-address');
+      }
+    }, 1000);
+  },
+  methods: {
+    selectAddress(v) {
+      const self = this;
+      this.avoidQuery = true;
+      setTimeout(() => {
+        self.avoidQuery = false;
+      }, 1200);
+      // eslint-disable-next-line vue/no-mutating-props
+      this.model.address = v;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.sc-address-autocomplete {
+  .q-field__native span {
+    display: none;
+  }
+
+  .q-select__dropdown-icon {
+    display: none;
+  }
+
+  .q-field__control {
+    font-size: 15px;
+  }
+}
+</style>
