c911a564a5320f2f3e73975d4528f397bef024f4
Vladichik <vvidavsky@gmail.com>
2021-09-08 11:23:58 +0300

    Begginning forgot pw form

--- client/src/components/auth/Forgot.vue
+++ client/src/components/auth/Forgot.vue
@@ -1,11 +1,30 @@
 <template>
-forgot
+  <q-form @submit="sendForgotPassword()">
+    <q-input filled
+             :placeholder="$t('email')"
+             v-model="userEmail"
+             :rules="[ val => val && val.length > 0 || $t('mandatory_field')]" />
+    {{userEmail}}
+  </q-form>
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
   name: 'Forgot',
-};
+  setup() {
+    const userEmail = ref('');
+    const sendForgotPassword = () => {
+
+    };
+
+    return {
+      userEmail,
+      sendForgotPassword,
+    };
+  },
+});
 </script>
 
 <style scoped>
