ddf9e16ed0da36392f645145a2c177305d844e03
Vladichik <vvidavsky@gmail.com>
2021-09-28 13:57:53 +0300

    Changing appId for solara

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -2,5 +2,5 @@ export const Constants = Object.freeze({
   AUTH_TOKEN_KEY: 'solara_token',
   ORVIBO_TOKEN_KEY: 'orvibo_token',
   ORVIBO_BASE_URL: 'https://oauthcn.orvibo.com/oauth/',
-  ORVIBO_CLIENT_ID: 'solara',
+  ORVIBO_CLIENT_ID: 'solara_en',
 });
--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -4,10 +4,10 @@ export default () => ({
   TOKEN_EXPIRY: '365d',
   ORVIBO_AUTH_URL: 'https://oauthcn.orvibo.com/oauth/token',
   ORVIBO_BASE_URL: 'https://open-api-de.orvibo.com/smart-home365',
-  // ORVIBO_BASE_URL_US: 'https://open-api.orvibo.com/smart-home365',
-  ORVIBO_CLIENT_ID: 'solara',
+  ORVIBO_CLIENT_ID: 'solara_en',
   ORVIBO_SECRET: '90c01f53bd55434cade9abead9a77ded',
   ORVIBO_AUTH: '8428ca11723d491e89268a7242f60653',
   WEATHER_API_SECRET: '55e03e48dcd04a19b87110610210809',
   WEATHER_API_URL: 'https://api.weatherapi.com/v1/current.json',
+  // ORVIBO_BASE_URL_US: 'https://open-api.orvibo.com/smart-home365',
 });
